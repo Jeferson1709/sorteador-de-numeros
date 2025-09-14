c2e7fc4cab825f248a59546e68dbbf13a72d1f42
Jeferson Alves <jefersonalvessantos62@gmail.com>
2025-09-13 21:35:38 -0300

    feat: Adicionando os valores gerados no html.

--- app.js
+++ app.js
@@ -14,18 +14,32 @@ function sortear() {
 
     //Laço For para preencher o array sorteados com números aleatórios.
     for (let i = 0; i < quantidade; i++) {
+
         // Atribuindo um valor aleatório 
         numero = obterNumeroAleatorio(de, ate);
+
+        //Enquando o valor já estiver no array, peça um novo valor.
+        while (sorteados.includes(numero)) {
+            // Atribuindo um valor aleatório.
+            numero = obterNumeroAleatorio(de, ate);
+
+        }
+
         //Adicionando o valor np array.
         sorteados.push(numero);
+
     }
-    //Exibindo os valores. 
-    alert(sorteados);
+
+    //Pegando um elemento do html.
+    let resultado = document.getElementById('resultado');
+
+    // adicionando um label com os valores aleatórios.
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
 }
 
 //Função que retorna um número aleatório.
 function obterNumeroAleatorio(min, max) {
-
+    //Lógica para retornar um número aleatório entre o min e o max.
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
